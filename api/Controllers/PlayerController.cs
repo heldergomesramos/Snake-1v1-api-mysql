2a6f6c27766ffe0f28b83247c854f74eb8d5bc1d
Pofinho <helderramos1414@gmail.com>
2024-10-01 21:47:35 +0100

    test xD

--- api/Controllers/PlayerController.cs
+++ api/Controllers/PlayerController.cs
@@ -13,15 +13,15 @@ namespace api.Controllers
     [ApiController]
     public class PlayerController : ControllerBase
     {
-        private readonly UserManager<Player> _userManager;
+        // private readonly UserManager<Player> _userManager;
         private readonly IPlayerService _playerService;
         private readonly ITokenService _tokenService;
         private readonly SignInManager<Player> _signInManager;
         private readonly ILogger<PlayerController> _logger;
-
-        public PlayerController(UserManager<Player> userManager, IPlayerService playerService, ITokenService tokenService, SignInManager<Player> signInManager, ILogger<PlayerController> logger)
+        //UserManager<Player> userManager
+        public PlayerController(IPlayerService playerService, ITokenService tokenService, SignInManager<Player> signInManager, ILogger<PlayerController> logger)
         {
-            _userManager = userManager;
+            //_userManager = userManager;
             _playerService = playerService;
             _tokenService = tokenService;
             _signInManager = signInManager;
@@ -69,34 +69,35 @@ namespace api.Controllers
                 };
 
                 _logger.LogInformation("Before user manager");
-                var createdUser = await _userManager.CreateAsync(user, dto.Password);
+                //var createdUser = await _userManager.CreateAsync(user, dto.Password);
                 _logger.LogInformation("After user manager");
 
-                if (createdUser.Succeeded)
-                {
-                    var roleResult = await _userManager.AddToRoleAsync(user, "User");
+                // if (createdUser.Succeeded)
+                // {
+                //     var roleResult = await _userManager.AddToRoleAsync(user, "User");
 
-                    if (roleResult.Succeeded)
-                    {
-                        var token = _tokenService.CreateToken(user);
+                //     if (roleResult.Succeeded)
+                //     {
+                //         var token = _tokenService.CreateToken(user);
 
-                        var responseDto = PlayerMappers.PlayerEntityToPlayerRegister(user, token);
+                //         var responseDto = PlayerMappers.PlayerEntityToPlayerRegister(user, token);
 
-                        return Ok(new { player = responseDto });
-                    }
-                    else
-                        return StatusCode(500, new { message = roleResult.Errors });
-                }
-                else
-                {
-                    return StatusCode(409, new { message = createdUser.Errors });
-                }
+                //         return Ok(new { player = responseDto });
+                //     }
+                //     else
+                //         return StatusCode(500, new { message = roleResult.Errors });
+                // }
+                // else
+                // {
+                //     return StatusCode(409, new { message = createdUser.Errors });
+                // }
             }
             catch (Exception e)
             {
                 _logger.LogInformation("Catch exception: " + e.Message);
                 return StatusCode(500, new { message = e });
             }
+            return StatusCode(500, new { message = "Lmao" });
         }
 
         [HttpPost("login")]
@@ -105,14 +106,15 @@ namespace api.Controllers
             if (!ModelState.IsValid)
                 return BadRequest();
 
-            var response = await _playerService.LoginPlayerAsync(dto);
+            //var response = await _playerService.LoginPlayerAsync(dto);
 
-            if (response == null)
-                return Unauthorized(new { message = "Invalid username or password." });
+            // if (response == null)
+            //     return Unauthorized(new { message = "Invalid username or password." });
 
-            if (PlayerManager.IsPlayerConnected(response.PlayerId))
-                return StatusCode(403, new { message = "Player is alredy connected." });
-            return Ok(response);
+            // if (PlayerManager.IsPlayerConnected(response.PlayerId))
+            //     return StatusCode(403, new { message = "Player is alredy connected." });
+            // return Ok(response);
+            return Ok("xD");
         }
 
         [HttpPost("guest")]
