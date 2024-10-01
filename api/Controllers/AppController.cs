a305d0bbd45e3740fb60ec1f46a11a15b146810f
Pofinho <helderramos1414@gmail.com>
2024-10-01 21:32:37 +0100

    simplified even further appcontroller

--- api/Controllers/AppController.cs
+++ api/Controllers/AppController.cs
@@ -1,7 +1,6 @@
 using api.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 
 namespace api.Controllers
 {
@@ -9,40 +8,37 @@ namespace api.Controllers
     [ApiController]
     public class AppController : ControllerBase
     {
-        private readonly ApplicationDBContext _context;
         private readonly ILogger<AppController> _logger;
 
-        // Inject the ApplicationDBContext and ILogger<AppController>
-        public AppController(ApplicationDBContext context, ILogger<AppController> logger)
+        public AppController(ILogger<AppController> logger)
         {
-            _context = context;
             _logger = logger;
         }
 
         [HttpGet("ping")]
-        public async Task<IActionResult> Ping()
+        public IActionResult Ping()
         {
             _logger.LogInformation("Ping!");
             Console.WriteLine("ping");
             return Ok(new { message = "Ping successful" });
-            try
-            {
-                _logger.LogInformation("Ping request received.");
-                Console.WriteLine("CONSOLE WRITE LINE PING");
-                // Fetch players from the database
-                //var players = await _context.Players.ToListAsync();
+            // try
+            // {
+            //     _logger.LogInformation("Ping request received.");
+            //     Console.WriteLine("CONSOLE WRITE LINE PING");
+            //     // Fetch players from the database
+            //     //var players = await _context.Players.ToListAsync();
 
-                // Log the count of players retrieved
-                //_logger.LogInformation($"Number of players retrieved: {players.Count}");
+            //     // Log the count of players retrieved
+            //     //_logger.LogInformation($"Number of players retrieved: {players.Count}");
 
-                return Ok(new { message = "Ping successful" });
-            }
-            catch (Exception ex)
-            {
-                // Log the exception
-                _logger.LogError(ex, "An error occurred while processing the ping request.");
-                return StatusCode(500, new { message = ex.Message });
-            }
+            //     return Ok(new { message = "Ping successful" });
+            // }
+            // catch (Exception ex)
+            // {
+            //     // Log the exception
+            //     _logger.LogError(ex, "An error occurred while processing the ping request.");
+            //     return StatusCode(500, new { message = ex.Message });
+            // }
         }
     }
 }
