8f0caf1e7b4027174fc6c43292951bb3d8f32169
Pofinho <helderramos1414@gmail.com>
2024-10-11 15:02:53 +0100

    Testing something

--- api/Hubs/GameHub.cs
+++ api/Hubs/GameHub.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace api.Hubs
 {
-    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [Authorize]
     public class GameHub(IPlayerService playerService, IHubContext<GameHub> hubContext) : Hub
     {
         private readonly IPlayerService _playerService = playerService;
